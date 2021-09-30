be2e8c1b71047213c5d763666cf87e37e3763584
gledson-ss <github@gledson.dev>
2021-09-30 20:43:33 -0300

    update method

---
+++ src/pages/index.tsx
@@ -0,0 +1,59 @@
+import { GetServerSideProps } from 'next'
+import Head from 'next/head'
+import styles from '../styles/Home.module.scss'
+import Image from 'next/image'
+import { stripe } from '../services/stripe'
+
+interface productProps { 
+  product:{
+    id: number;
+    price: number;
+    name: string;
+    image: string;
+    description: string;
+  }
+}
+
+export default function Home({ product }: productProps) {
+  
+  return (
+    <>
+    <Head>
+      <title>Home</title>
+    </Head>
+    <div className={styles.header}>
+      <div className={styles.textH4}>
+        <h1 className={styles.textHeader}>
+          ola
+        </h1>
+      </div>
+    </div>
+    <main className={styles.mainTag}>
+      <div className={styles.card}>
+        {product.name}
+        <Image src={product.image} width="120px" height="100px" alt={product.description}/>
+      </div>
+    </main>
+    </>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async () =>{
+  const priceProduct = await stripe.prices.retrieve('price_1JfXDVHBcM4EyCHKmHpuJph7', {
+    expand: ['product']
+  })
+  const infoProduct = await stripe.products.retrieve('prod_KKBatjJwuU8UoB')
+
+  const product = {
+    id: priceProduct.id,
+    price: priceProduct.unit_amount / 100,
+    name: infoProduct.name,
+    image: infoProduct.images[0],
+    description: infoProduct.description
+  }
+  return {
+    props: {
+      product
+    }
+  }
+}
